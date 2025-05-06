ebb75a39ed3afcd0abfda8a6203d0a594242725b
Alexander-Luk <lukinalex1809@gmail.com>
2025-05-06 23:14:13 +0300

    Add files via upload

--- ТГ-бот.py
+++ ТГ-бот.py
@@ -34,7 +34,8 @@ async def start_command(message: types.Message) -> None:
             types.KeyboardButton(text="О нас")
         ],
         [
-            types.KeyboardButton(text="Расчёт стоимости товара")
+            types.KeyboardButton(text="Расчёт стоимости товара"),
+            types.KeyboardButton(text="Товары")
         ]
     ]
     keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
@@ -80,6 +81,25 @@ async def about_you_handler(message: types.Message) -> None:
 @dp.message(F.text == 'Напишите цену товара в юанях, наш бот рассчитает его цену в рублях')
 async def about_you_handler(message: types.Message) -> None:
     await message.answer("123")
+    a = message.send_copy(chat_id=message.chat.id)
+
+
+@dp.message(F.text == 'Товары')
+async def about_you_handler(message: types.Message) -> None:
+    kb = [
+        [
+            types.KeyboardButton(text="Тип 1"),
+            types.KeyboardButton(text="Тип 2"),
+            types.KeyboardButton(text="Тип 3")
+        ],
+        [
+            types.KeyboardButton(text="Тип 4"),
+            types.KeyboardButton(text="Тип 5"),
+            types.KeyboardButton(text="Тип 6")
+        ]
+    ]
+    product_types_keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+    await message.answer("Выберите категорию товара", reply_markup=product_types_keyboard)
     a = message.send_copy(chat_id=message.chat.id)
 
 
